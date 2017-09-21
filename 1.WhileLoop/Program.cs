87ee1b5886adddff62b274b58c86678a53da6907
Mitali Pattani <sonarmitali19@gmail.com>
2017-09-21 12:19:03 -0700

    Add project files.

---
+++ 1.WhileLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1.WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entry controlled loop
+            //1. Intialization
+            //2. Condition
+            //3. Increment/ Decrement
+            int i = 51;
+            Console.WriteLine("series from 1 to 50");
+            while(i<=50)
+            {
+                Console.Write("\t{0}", i);
+                i++;
+            }
+            //?how many time??? 50 times
+
+            //exit Controlled loop Do...While();
+            int j = 51;
+            Console.WriteLine();
+            Console.WriteLine("Using Do while loop series from 1 to 50");
+            do
+            {
+                Console.Write("\t{0}", j);
+                j++;
+            } while (j<=50);
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 2.ForloopFactorial/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _2.ForloopFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Factorial 2! = 2 *1 = 2
+            //Factorial 3! = 3*2*1 = 6
+            //Factorial 4! = 4*3*2*1 = 24
+            // Factorial 5! = 5*4*3*2*1 = 120
+
+            //intialization ; condition; increment/decrement
+            int n = 20;
+            decimal factorial = 1;
+            for(int i=n; i>=1; i-- )
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("{0}! = {1}", n, factorial);
+            //variation of for loop
+            //For-each loop
+            Console.WriteLine("For each example");
+            string[] dayName = new string[]
+            {
+                "MONDAY","TUESDAY","WEDNESDAY",
+                "THURSDAY","FRIDAY","SATURDAY",
+                "SUNDAY"
+            };
+            ///datatype variable keyword(in) collection_name
+            //foreach is used over collections i.g list, arrays, 
+            // maps,sets
+            foreach(string day in dayName)
+            {
+                Console.WriteLine(day);
+
+            }
+    
+
+            Console.Read();
+
+
+        }
+    }
+}
